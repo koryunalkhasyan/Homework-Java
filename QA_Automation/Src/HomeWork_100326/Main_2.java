9550014f3195db0193a586f8bac4084a8b218b91
Koryun Alkhasyan <koryunalkhasyaninfo@gmail.com>
2026-03-27 17:32:32 +0400

    I review code

--- QA_Automation/Src/HomeWork_100326/Main_2.java
+++ QA_Automation/Src/HomeWork_100326/Main_2.java
@@ -2,29 +2,36 @@ package QA_Automation.Src.HomeWork_100326;
 
 public class Main_2 {
     public static void main (String[] args){
-        Dog dog = new Dog();
-        Cat cat = new Cat();
+        Dog dog = new Dog("Rex");
+        Cat cat = new Cat("Molly");
         dog.makeSound();
         cat.makeSound();
     }
 }
 class Animal {
     String name;
-
+    public Animal (String name){
+        this.name = name;
+    }
     public void makeSound(){
+        System.out.println(name + " ");
     }
 }
 class Dog extends Animal{
+    public Dog(String name) {
+        super(name);
+    }
     @Override
     public void makeSound(){
-        super.makeSound();
-        System.out.println("Haf Haf");
+        System.out.println(name + " is Haf Haf");
     }
 }
 class Cat extends Animal {
+    public Cat(String name){
+        super(name);
+    }
     @Override
     public void makeSound(){
-        super.makeSound();
-        System.out.println("Myu Myu");
+        System.out.println(name + " is Meow Meow");
     }
 }
